3c4244c66d900e0cfd7d3134cdfa0ec4adc89279
Tisha Anderson <136303804+tishaanderson@users.noreply.github.com>
2023-10-05 22:55:23 -0500

    add dayjs code to script.js to display current day and time on calendar

--- Develop/script.js
+++ Develop/script.js
@@ -20,4 +20,7 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
+
+  var currentDate = dayjs().format('dddd, MMMM D, YYYY h:mm A');
+  $('#currentDay').text(currentDate);
 });
