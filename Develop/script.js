00ba7d0318b024a86d22378bb44640203a879757
Tisha Anderson <136303804+tishaanderson@users.noreply.github.com>
2023-10-08 23:25:05 -0500

    finalized js by adding the events to the local storage and console along with their designated time slot, reorganized js so it would flow better according to the notes

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,7 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-
+$(document).ready(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -9,25 +9,24 @@
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
   //
-$(document).ready(function () {
-  var saveButton = $('#save-button');
-  var eventDescription = $('.description');
+  function saveEvent(hour, eventText) {
+    localStorage.setItem("event-" + hour, eventText);
+  }
 
-  var savedEventDescription = localStorage.getItem('eventDescription');
+  currentTime();
+  updateBlockClasses();
+  loadEvents();
 
-  saveButton.on('click', function (event) {
-    event.preventDefault();
+  $(".saveBtn").on("click", function () {
+    var hour = $(this).parent().attr("id");
+    var eventText = $(this).siblings(".description").val();
 
-    
-    var eventText = eventDescription.val();
+    console.log(hour);
+    console.log(eventText);
 
-    if (!eventText) {
-      console.log('Please enter an event to save for this time-slot.');
-      return;      
-    }
+    saveEvent(hour, eventText);
+  });
 
-    localStorage.setItem('eventDescription', eventText);
-  })
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
@@ -37,15 +36,15 @@ $(document).ready(function () {
   function updateBlockClasses() {
     var currentHour = dayjs().hour();
 
-    $('.time-block').each(function () {
-      var blockHour = parseInt($(this).attr('data-hour'));
+    $(".time-block").each(function () {
+      var blockHour = parseInt($(this).attr("data-hour"));
 
       if (blockHour < currentHour) {
-        $(this).removeClass('future present').addClass('past');
+        $(this).removeClass("future present").addClass("past");
       } else if (blockHour === currentHour) {
-        $(this).removeClass('past future').addClass('present');
+        $(this).removeClass("past future").addClass("present");
       } else {
-        $(this).removeClass('past present').addClass('future');
+        $(this).removeClass("past present").addClass("future");
       }
     });
   }
@@ -54,19 +53,25 @@ $(document).ready(function () {
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
-  // TODO: Add code to display the current date in the header of the page.
+  function loadEvents() {
+    $(".time-block").each(function () {
+      var blockHour = $(this).attr("data-hour");
+      var eventText = localStorage.getItem("event-" + blockHour);
 
-  
-
-  setInterval(currentTime, 1000);
-  
-  function currentTime() {
-    var currentDate = dayjs().format('dddd, MMMM D, YYYY h:mm A');
-    $('#currentDay').text(currentDate);
-
-    currentDate.innerHTML = "currentDate.toLocaleTimeString()";  
+      if (eventText) {
+        $(this).find(".description").val(eventText);
+      }
+    });
   }
 
+  // TODO: Add code to display the current date in the header of the page.
 
+  setInterval(currentTime, 1000);
 
+  function currentTime() {
+    var currentDate = dayjs().format("dddd, MMMM D, YYYY h:mm A");
+    $("#currentDay").text(currentDate);
+
+    currentDate.innerHTML = "currentDate.toLocaleTimeString()";
+  }
 });
